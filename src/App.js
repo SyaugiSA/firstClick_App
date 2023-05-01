ec6ce3a348b5daae0871875dfa9d5778caa33455
SyaugiSA <syaugiamar9@gmail.com>
2023-05-01 14:10:57 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './app.css';
-import Sidebar from './Components/SideBar Section/Sidebar';
-import Body from './Components/Body Section/Body';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Components/SideBar Section/Sidebar";
+import Body from "./Components/Body Section/Body";
 
-import Top from './Components/Body Section/Top Section/Top';
+import Top from "./Components/Body Section/Top Section/Top";
 
 const App = () => {
-    return (
-        <div className="container">
-            <Sidebar />
-            <Body />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Sidebar />
+      <Body />
+    </div>
+  );
 };
 
 export default App;
